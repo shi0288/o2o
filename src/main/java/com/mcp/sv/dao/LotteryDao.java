3a5b5dc60544e6864ef0d94326c54592a4cdabff
shiqingmao <shi_qingmao@126.com>
2015-06-23 17:47:17 +0800

    update

--- src/main/java/com/mcp/sv/dao/LotteryDao.java
+++ src/main/java/com/mcp/sv/dao/LotteryDao.java
@@ -314,11 +314,10 @@ public class LotteryDao {
                 DBObject newCzOrder = (DBObject) JSON.parse(czOrderStr);
                 newCzOrder.put("status", 1200);
                 int res = MongoUtil.update(MongoConst.MONGO_ALIPAY, czOrder, newCzOrder);
-                logger.error("res:"+res);
                 if (res == 1) {
+                    logger.error("开始充值");
                     String userName = (String) czOrder.get("userName");
-                    int money = (int) czOrder.get("money");
-                    logger.error("进入开始充值");
+                    int money = Integer.parseInt((String) czOrder.get("money"));
                     LotteryDao.recharge(userName, money,true,out_trade_no);
                     return true;
                 } else {

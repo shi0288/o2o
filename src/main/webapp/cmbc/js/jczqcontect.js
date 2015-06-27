9fdbf354e0359b3130a7cfbe92f0b81b5d64c930
wangcz2013 <wangcz2013@126.com>
2015-06-27 17:47:30 +0800

    update

--- src/main/webapp/cmbc/js/jczqcontect.js
+++ src/main/webapp/cmbc/js/jczqcontect.js
@@ -212,7 +212,7 @@ function submitJc() {
         'number': numbers,
         'multiple': $("#beishu").val(),
         "presetTerminal":"0000",
-        "outerId":new Date().getTime()+Math.random()*(1000-390)+390,
+        "outerId":new Date().getTime()+Math.random().toString(36).substr(8),
         "auditTime":new Date().format("yyyy-MM-dd hh:mm:ss")
     }
     tickets.push(ticket);
@@ -220,7 +220,7 @@ function submitJc() {
 
     var order = {
         'amount': amount,
-        'outerId':new Date().getTime()+Math.random()*(1000-390)+390,
+        'outerId':new Date().getTime()+Math.random().toString(36).substr(8),
         'tickets': tickets
     };
 
